70b246b6524ce2b511f72e2296c422ae41c3f56f
shaikhserias2511 <azruddin20oct1999@gmail.com>
2024-06-18 20:04:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div>
+      <Button variant="text">Text</Button><br /><br />
+      <Button variant="contained">Contained</Button><br /><br />
+      <Button variant="outlined">Outlined</Button>
+
+    </div>
+  );
+}
+
+export default App;
